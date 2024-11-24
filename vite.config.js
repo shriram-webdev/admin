594232baba1f8b34436071608044e2df0643fb33
shriram-webdev <shriram7927@gmail.com>
2024-11-24 17:28:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/admin/',  // This ensures assets are loaded from /admin/ subdirectory
   plugins: [react()],
 })
